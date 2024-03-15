d9e9a1374dc9d10100df20ef75bcb294c4308468
lactaid <gandresemanuel@gmail.com>
2024-03-15 16:35:23 -0700

    first deploy

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', async (req,res) => {
+
+    res.render('home')
+})
+
+
+app.listen(4000, ()=>{
+    console.log('hello')
+})
+
