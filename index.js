0cbd5a211349754103d340d57c69c49647a16c45
lactaid <gandresemanuel@gmail.com>
2024-03-15 17:17:03 -0700

    greymatter

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', async (req,res) => {
 
