53df0bec5a42dfb18874ad839311cc39bcd94794
lactaid <gandresemanuel@gmail.com>
2024-03-22 15:54:07 -0700

    videos

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var os = require('os');
 
+const PORT = 4000;
+let ip = '0.0.0.0'; 
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +21,22 @@ app.get('/stream', async (req,res) => {
 })
 
 
-app.listen(4000, ()=>{
-    console.log('hello')
-})
+var ips = os.networkInterfaces();
+Object
+  .keys(ips)
+  .forEach(function(_interface) {
+     ips[_interface]
+      .forEach(function(_dev) {
+        if (_dev.family === 'IPv4' && !_dev.internal) ip = _dev.address 
+      }) 
+  });
+
+
+
+  // From https://gist.github.com/nfort/a29404417452a06ed6e31b3032c7e42b
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://${ip}:${PORT}`);
+});
 
